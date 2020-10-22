edcdc326457e1e5041dd36caa929c6ee5228d1a8
Dreamweaver2k <chandlerault@gmail.com>
2020-10-22 12:45:36 -0400

    updated prediction values

--- stock_pred.py
+++ stock_pred.py
@@ -5,7 +5,8 @@ import numpy as np
 import os, sys
 from matplotlib import pyplot as plt
 from keras.utils import np_utils
-from keras import preprocessing
+from sklearn import preprocessing
+
 def normalize(matrix):
     matrix[:,:-1] =  (matrix[:,:-1]-np.min(matrix[:,:-1]))/(np.max(matrix[:,:-1]) - np.min(matrix[:,:-1]))
     matrix[:,-1:] = (matrix[:,-1:]-np.min(matrix[:,-1:]))/(np.max(matrix[:,-1:]) - np.min(matrix[:,-1:]))
@@ -14,11 +15,12 @@ def normalize(matrix):
 
 def baseline_model():
     model = Sequential()
-    model.add(LSTM(units=400))
-    model.add(Dropout(0.3))
+    model.add(LSTM(units=200, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(Dense(8))
     #model.add(Dense(4))
     model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='mae', optimizer='adam', metrics=['mape'])
     return model
 
 
@@ -34,16 +36,19 @@ def createModel(dataDir, savefile, epochs, batchsize):
         data = pd.read_csv(dir + '\\' + sets[0])
         train_size = int(.8*data.shape[0]) # 80% of data for training
         x = data.iloc[:,1:6].to_numpy() # y = data.loc[1:,'Close']
-        x = normalize(x)
+        scale = preprocessing.MinMaxScaler(feature_range=(0,1))
+        x[:,:-1] = scale.fit_transform(x[:,:-1])
+        x[:, -1:] = scale.fit_transform(x[:,-1:])
 
         # Parse data
         x_new_train = []
         y_new_train = []
-        for i in range(60, x.shape[0]-30):
-            x_new_train.append(x[i-60:i, :])
-            y_new_train.append(np.average(x[i:i+30,3]) > np.average(x[i,3]))
+        for i in range(30, x.shape[0]-20):
+            x_new_train.append(x[i-30:i, :])
+            y_new_train.append(np.average(x[i:i+20,3]))
         x_train, y_train = np.array(x_new_train), np.array(y_new_train)
-        y_train = np.where(y_train > 0, 1, 0)
+
+        #y_train = np.where(y_train == True, 1, 0)
         x_test, y_test = x_train[train_size:, :], y_train[train_size:]
         x_train, y_train = x_train[:train_size, :], y_train[:train_size]
 
@@ -57,17 +62,20 @@ def createModel(dataDir, savefile, epochs, batchsize):
             data = pd.read_csv(dir +'\\'+ sets[i])
             train_size = int(.8*data.shape[0])
             x = data.iloc[:,1:6].to_numpy()
-            x = normalize(x)
+            y = x[:, 3]
+
+            x[:, :-1] = scale.fit_transform(x[:, :-1])
+            x[:, -1:] = scale.fit_transform(x[:, -1:])
 
             # Parse Data
             x_new_train = []
             y_new_train = []
-            for i in range(60, x.shape[0]-30):
-                x_new_train.append(x[i - 60:i, :])
-                y_new_train.append(np.average(x[i:i+30,3]) > np.average(x[i:i,3]))
+            for i in range(30, x.shape[0] - 20):
+                x_new_train.append(x[i - 30:i, :])
+                y_new_train.append(np.average(x[i:i + 20, 3]))
 
             x_tr, y_tr = np.array(x_new_train), np.array(y_new_train)
-            y_tr = np.where(y_tr > 0, 1, 0)
+
             x_te, y_te = x_tr[train_size:, :], y_tr[train_size:]
             x_tr, y_tr = x_tr[:train_size, :], y_tr[:train_size]
 
@@ -85,12 +93,20 @@ def createModel(dataDir, savefile, epochs, batchsize):
     model.fit(x_train, y_train, epochs=epochs,batch_size=batchsize,verbose=2)
     scores = model.evaluate(x_test, y_test, verbose=2)
     print("LSTM Error: %.2f%%" % (100-scores[1]*100))
+
+    scores = model.predict(x_test)
+    plt.plot(scores, label='Projected Price')
+    plt.plot(y_test, label='Actual Price')
+    # plt.plot(comparison[60:], label='Previous Close')
+    plt.legend()
+    plt.show()
+
     model.save(savefile)
     return
 
 
 dir = 'C:\\Users\\Chand\\Desktop\\IndependentProjects\\Learning\\Machine_Learning\\archive\\Stocks'
 savefile = 'modelbinary.h5'
-epochs = 3
-batch = 200
+epochs = 5
+batch = 100
 createModel(dir, savefile,epochs,batch)
